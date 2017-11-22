2a7aa88aa533316cb5c85722138cc4ca21c13d33
CimLabDana <edan100@gmail.com>
2017-11-22 19:34:15 +0200

    update main

--- main.py
+++ main.py
@@ -5,21 +5,26 @@ from kivy.properties import ObjectProperty
 import sys
 
 
+USER = 'cimlab'
+PASS = 'CimLab'
+DB = 'checkIt'
+
 class InputForm(BoxLayout):
     'Input form entrty point'
-    name = ObjectProperty()  # Variables fro KV file.
+    student_name = ObjectProperty()  # Variables fro KV file.
+    course=ObjectProperty()
     grade = ObjectProperty()
     ip = ObjectProperty()
 
     def connect(self):
         'Creates database and table if not exists yet'
-        con = mdb.connect(host=self.ip.text, user='cimlab', passwd='CimLab')
+        con = mdb.connect(host=self.ip.text, user=USER, passwd=PASS)
         cur = con.cursor()
         cur.execute('CREATE DATABASE if not exists checkIt;')
         con.commit()  # Makes transaction
         con.close()  # Close transaction
         # Connects get the ip address from user
-        con = mdb.connect(host=self.ip.text, user='cimlab', passwd='CimLab', db='checkIt')
+        con = mdb.connect(host=self.ip.text, user=USER, passwd=PASS, db=DB)
         cur = con.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS Programs \
                     (Id INTEGER(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, \
@@ -29,33 +34,41 @@ class InputForm(BoxLayout):
 
     def insert(self):
         'Inserts data to database'
-        con = mdb.connect(host=self.ip.text, user='cimlab', passwd='CimLab', db='checkIt')
+        con = mdb.connect(host=self.ip.text, user=USER, passwd=PASS, db=DB)
         cur = con.cursor()  # Making cursor
-        if self.name.text.isalpha() and self.grade.text.isdigit():  # Check input
-            cur.execute("INSERT INTO Programs (Lang,Grade) VALUES ('%s',%d)"
-                        % (self.name.text, int(self.grade.text)))  # Inserts user input to database
+        if self.student_name.text.isalpha() and self.grade.text.isdigit():  # Check input
+            cur.execute("INSERT INTO Programs (student_name,course, grade) VALUES ('%s',%d)"
+                        % (self.student_name.text, self.course.text, int(self.grade.text)))  # Inserts user input to database
             con.commit()
             con.close()
         else:
-            self.name.text = self.grade.text = 'Please type valid data'  # Error msg for incorrect input.
+            self.student_name.text = self.grade.text = 'Please type valid data'  # Error msg for incorrect input.
             return
-        self.name.text = self.grade.text = ''  # Clean text boxes
-        self.name.focus = True  # Set focus on student name text box for another insertion.
+        self.student_name.text = self.grade.text = ''  # Clean text boxes
+        self.student_name.focus = True  # Set focus on student name text box for another insertion.
 
     def displayRecords(self):
         'Display new form on screen'
         self.clear_widgets()  # Clears InputForm
         self.add_widget(ShowRecords(self.ip.text))  # Adds ShowRecords as new form
 
+    def out(self):
+        'Close application'
+        sys.exit()
+
 
 class ShowRecords(BoxLayout):
     'Shows the content of database'
     res = ObjectProperty()
+    student_name = ObjectProperty()  # Variables fro KV file.
+    course = ObjectProperty()
+    grade = ObjectProperty()
+    ip = ObjectProperty()
 
     def __init__(self, ip):
         'Builder of the class'
         super(ShowRecords, self).__init__()  # Call builder of BoxLayout because we don't use default builder
-        con = mdb.connect(host=ip, user='cimlab', passwd='CimLab', db='checkIt')
+        con = mdb.connect(host=self.ip.text, user=USER, passwd=PASS, db=DB)
         cur = con.cursor()
         cur.execute("SELECT * FROM Programs")  # Quering database
         rows = cur.fetchall()  # Retrive all rows from query.
@@ -68,7 +81,38 @@ class ShowRecords(BoxLayout):
             self.res.text = 'No Records'  # Error msg
         else:
             for r in rows:
-                self.res.text += r[1] + ' ' + str(r[2]) + '\n'  # Make readable msg
+                    if len(r)>3:
+                        self.res.text += r[1] +''+r[2]+''+str(r[3])+ '\n'
+                    else:
+                        self.res.text += r[0] + ' ' + str(r[1]) + '\n'  # Make readable msg
+    def count_users(self):
+        con = mdb.connect(host=self.ip.text, user=USER, passwd=PASS, db=DB)
+        cur = con.cursor()
+        cur.execute("SELECT course,COUNT(student_name) FROM Programs GROUP BY course")
+        rows=cur.fetchall()
+        self.show(rows)
+
+    def user_avg(self):
+        con = mdb.connect(host=self.ip.text, user=USER, passwd=PASS, db=DB)
+        cur = con.cursor()
+        cur.execute("select a course, COUNT(student_name) FROM Programs group By course")
+        rows=cur.fetchall()
+        self.show(rows)
+
+
+    def good_students(self):
+        con = mdb.connect(host=self.ip.text, user=USER, passwd=PASS, db=DB)
+        cur = con.cursor()
+        cur.execute("SELECT student_name, grade FROM Programs\
+         WHERE grade=(SELECT MAX(grade) FROM Programs WHERE course= 'Math')\
+         AND course='Math'")
+        rows=cur.fetchall()
+        self.show(rows)
+
+    def display_input_data(self):
+        self.clear_widgets()
+        self.add_widget(InputForm())
+
 
     def out(self):
         'Close application'
