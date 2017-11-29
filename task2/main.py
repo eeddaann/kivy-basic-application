8394dee67e5d51ff8230cf6fd9ef935b32a705e1
eeddaann <edan100@gmail.com>
2017-11-30 01:21:09 +0200

    update task 2

--- task2/main.py
+++ task2/main.py
@@ -3,7 +3,8 @@ from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from plyer import accelerometer
-
+import nxt.locator
+import nxt.motor
 
 class InputForm(BoxLayout):
 	"Expose KV file Id's to python"
@@ -12,39 +13,29 @@ class InputForm(BoxLayout):
 	def __init__(self):
 		"Form constructor"
 		super(InputForm, self).__init__()
-		self.sensorEnabled = False
+		self.roboLego = nxt.locator.find_one_brick(name=self.brickname.text)
+		self.rMotor = nxt.Motor(self.roboLego, nxt.motor.PORT_C)
+		self.lMotor = nxt.Motor(self.roboLego, nxt.motor.PORT_A)
 
+	def forward(self):
+		self.rMotor.run()
+		self.lMotor.run()
 
-	def do_toggle(self):
-		try:
-			if not self.sensorEnabled:
-				accelerometer.enable()
-				# Enabling repetitive calls to get_acceleration
-				# method every quarter of a second
-				Clock.schedule_interval(self.get_acceleration, 1 / 4.)
-				self.sensorEnabled = True
-				# Change button text
-				self.but.text = "Stop Accelerometer"
-			else:
-				accelerometer.disable()
-				Clock.unschedule(self.get_acceleration)
-				self.sensorEnabled = False
-				self.but.text = "Start Accelerometer"
-		except NotImplementedError:
-			self.lStatus.text = "Accelerometer is not implemented for your platform"
+	def stop(self):
+		self.rMotor.brake()
+		self.lMotor.brake()
 
+	def left(self):
+		self.lMotor.run()
+		self.rMotor.brake()
 
-	def get_acceleration(self, dt):
-		val = accelerometer.acceleration[:3]
-		if (not val == (None, None, None)):
-			self.lX.text = "X: " + str(round(val[0], 2))
-			self.lY.text = "Y: " + str(round(val[1], 2))
-			self.lZ.text = "Z: " + str(round(val[2], 2))
+	def right(self):
+		self.lMotor.brake()
+		self.rMotor.run()
 
-
-class AccelApp(App):
+class KivyApp(App):
 	pass
 
 
 if __name__ == '__main__':
-	AccelApp().run()
+	KivyApp().run()
